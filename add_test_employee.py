6663cc89a74b701c59bb8db97325d639963b5e07
WINGOGO <wingogo80428@hotmail.com>
2025-08-21 13:38:53 +0800

    修正時間限制檢查功能 - 恢復正確的時間限制邏輯

--- add_test_employee.py
+++ add_test_employee.py
@@ -7,7 +7,14 @@ cursor = conn.cursor()
 # 新增測試員工
 cursor.execute('''
     INSERT INTO employees (employee_id, name, department_id, shift_type, preferred_language)
-    VALUES ('IGA1-02849', '測試員工', 1, 'day', 'zh')
+    VALUES ('IGA1-02849', '測試員工', 1, 'day', 'zh'),
+    ('IGA1-01657', '測試員工2', 1, 'day', 'zh'),
+    ('IGA1-02851', '測試員工3', 1, 'day', 'zh'),
+    ('IGA1-02852', '測試員工4', 1, 'day', 'zh'),
+    ('IGA1-02853', '測試員工5', 1, 'day', 'zh'),
+    ('IGA1-02854', '測試員工6', 1, 'day', 'zh'),
+    ('IGA1-02855', '測試員工7', 1, 'day', 'zh'),
+    ('IGA1-02856', '測試員工8', 1, 'day', 'zh'),
 ''')
 
 conn.commit()
